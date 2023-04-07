de81624dad6e100126ac80d20b20dba4c9974504
MuhammadMahbub <muhammadmahbub07@gmail.com>
2023-04-07 15:39:11 +0600

    search js

---
+++ search_table_JS.php
@@ -0,0 +1,18 @@
+
+
+<script>
+        $(document).ready(function () {
+            // Table Search
+            $('.table_search').on('input', function(){
+                var tableSearchValue = $(this).val();
+                $(this).closest(".card").find(".table tbody tr").each(function(){
+                    if($(this).text().search(new RegExp(tableSearchValue, "i")) < 0){
+                        $(this).hide();
+                    }
+                    else{
+                        $(this).show();
+                    }
+                });
+            });
+        });
+    </script>
